4ce19dce972c4096c73d9cda71830814e0c3bffb
邱勁豪 <a145621308@gmail.com>
2022-05-27 16:28:09 +0800

    Homework1 Gui guess game

--- src/main/java/com/fju/ui/GuessFrame.java
+++ src/main/java/com/fju/ui/GuessFrame.java
@@ -4,23 +4,40 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 public class GuessFrame extends JFrame {
+
     //Fields
     JButton button = new JButton("ok");
     JLabel label = new JLabel("123");
+    JTextField number = new JTextField(7);
+    Random random = new Random();
+    int n = random.nextInt(10) + 1;
     //Constructors
     public GuessFrame(){
         super();
+        System.out.println(n);
         setSize(600,400);
         setLocation(300,200);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                label.setText("Hi");
-            }
+                int num = Integer.parseInt(number.getText());
+                System.out.println(num);
+                if (num == n) {
+                    label.setText("bingo");
+                }else if (num < n){
+                    label.setText("bigger");
+                }else {
+                    label.setText("smaller");
+                }
+                }
         });
+
+
         setLayout(new FlowLayout());
+        add(number);
         add(button);
         add(label);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -34,6 +51,5 @@ public class GuessFrame extends JFrame {
 
     public static void main(String[] args) {
         GuessFrame guessFrame = new GuessFrame();
-
     }
 }
